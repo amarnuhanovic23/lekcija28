c562f996651c0ae2a7c62b3ba451d9981e874527
amarnuhanovic23 <amarmanji1234@gmail.com>
2026-05-07 21:03:34 +0200

    popravka

--- SHAPES_CHALLENGE/js/main.js
+++ SHAPES_CHALLENGE/js/main.js
@@ -1,16 +1,32 @@
-var button_circle = document.getElementById("circle_btn");
-var button_square = document.getElementById("square_btn");
-var button_triangle = document.getElementById("triangle_btn");
-var circle = document.getElementById("circle");
-var square = document.getElementById("square");
-var triangle = document.getElementById("triangle");
+var shape1 = document.getElementById("circle_btn");
+var shape2 = document.getElementById("square_btn");
+var shape3 = document.getElementById("triangle_btn");
+var circle = document.getElementById("circlediv");
+var square = document.getElementById("squarediv");
+var triangle = document.getElementById("trianglediv");
 
-button_circle.onclick = function() {
-    circle.setAttribute("class", "circle");
+shape1.onclick = function() {
+    circle.removeAttribute("class", "hide");
+    circle.setAttribute("class", "first");
 }
-button_square.onclick = function() {
-    square.setAttribute("class", "square");
+shape2.onclick = function() {
+    square.removeAttribute("class", "hide");
+    square.setAttribute("class", "second");
 }
-button_triangle.onclick = function() {
-    triangle.setAttribute("class", "triangle");
+shape3.onclick = function() {
+    triangle.removeAttribute("class", "hide");
+    triangle.setAttribute("class", "third");
+}
+
+shape1.onmouseleave = function() {
+    circle.removeAttribute("class", "first");
+    circle.setAttribute("class", "hide");
+}
+shape2.onmouseleave = function() {
+    square.removeAttribute("class", "second");
+    square.setAttribute("class", "hide");
+}
+shape3.onmouseleave = function() { 
+    triangle.removeAttribute("class", "third");
+    triangle.setAttribute("class", "hide");
 }
